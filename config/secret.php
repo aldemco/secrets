fd0d46ec44e3511f20f371e74f8f58ac81054c62
Aldemco <krdluad@gmail.com>
2022-08-14 13:25:02 +0300

    add config

---
+++ config/secret.php
@@ -0,0 +1,14 @@
+<?php
+
+// config for Aldemco/Secrets
+return [
+    'length' => 6,
+    'secret_generator' => Aldemco\Secrets\SecretGenerator::class,
+    'code_hasher' => Aldemco\Secrets\SecretHasher::class,
+    'is_crypt' => false,
+    'attemps' => 0,
+    'store_until_minutes' => 50000,
+    'valid_until_minutes' => 10,
+    'valid_from_minutes' => 0,
+    'multiple_limit' => 10,
+];

d14b09fcd2d0fb2507faa03918c6dc046a87296d
Aldemco <krdluad@gmail.com>
2022-08-14 13:08:10 +0300

    add migrations

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Aldemco\Secrets\Database\Factories;
+
+use Aldemco\Secrets\Models\Secret;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+// class ModelFactory extends Factory
+// {
+//     protected $model = Secret::class;
+
+//     public function definition()
+//     {
+
+//     }
+// }

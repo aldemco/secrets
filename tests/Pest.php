f846de61708ee90d8c701f3b60a9654d64f41846
Aldemco <krdluad@gmail.com>
2022-08-14 17:29:19 +0300

    create tests class

---
+++ tests/Pest.php
@@ -0,0 +1,5 @@
+<?php
+
+use Aldemco\Secrets\Tests\TestCase;
+
+uses(TestCase::class)->in(__DIR__);
---
+++ tests/TestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Aldemco\Secrets\Tests;
+
+use Aldemco\Secrets\SecretsServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Aldemco\\Secrets\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            SecretsServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+
+        $migration = include __DIR__.'/../database/migrations/create_confirm_code_table.php.stub';
+        $migration->up();
+    }
+}

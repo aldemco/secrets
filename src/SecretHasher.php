629df8e7df5b94ad79cdb464255590791166fbb0
Aldemco <krdluad@gmail.com>
2022-08-19 23:09:09 +0300

    optimization

--- src/SecretHasher.php
+++ src/SecretHasher.php
@@ -7,11 +7,8 @@ use Illuminate\Support\Facades\Hash;
 
 class SecretHasher implements SecretHasherContract
 {
-    private static $count = 0;
-
     public function encrypt($secret): string
     {
-        self::$count++;
         return Hash::make($secret);
     }
 
